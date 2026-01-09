4699c033aa8a23dc1be851666374104ce07bddeb
vieramaster <vieramaster@hotmail.com>
2026-01-09 17:03:28 -0300

    components: delete irrelevant components

--- components/ui/error-page.tsx
+++ components/ui/error-page.tsx
@@ -6,6 +6,6 @@ export const ErrorPage = ({message}:ErrorPageProps) =>(
         <div className="flex flex-col items-center justify-center min-h-[50vh] gap-4">
       <h2 className="text-xl font-bold">No se pudo cargar el artículo</h2>
       <p className="text-gray-500">{message}</p>
-
+  
     </div>
 )
